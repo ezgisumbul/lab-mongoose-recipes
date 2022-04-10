9a943c3582d9a3a1e4a93b1d9966a0823c8dbad6
ezgisumbul <ezgisumbul@gmail.com>
2022-04-10 16:09:14 +0200

    done'

--- index.js
+++ index.js
@@ -10,14 +10,53 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // console.log('List of recipes:', data);
+    // Recipe.create(data[0]);
+    return Recipe.create({
+      title: 'Musakka',
+      level: 'Amateur Chef',
+      ingredients: [
+        'Eggplants',
+        'Tomatoes',
+        'Minced meat',
+        'Tomato paste',
+        'Salt & Pepper'
+      ],
+      cuisine: 'Aegean',
+      dishType: 'main_course',
+      image:
+        'https://www.tuerkisch-rezepte.de/wp-content/uploads/Patl%C4%B1can-Musakka-auberginen-mousakka-rezept.png',
+      duration: 90
+    });
   })
-  .catch(error => {
+  .then((response) => {
+    console.log(response.title);
+    Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      {
+        title: 'Rigatoni alla Genovese'
+      },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(() => {
+    console.log('Recipe updated');
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Recipe deleted');
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,64 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 140
+    },
+
+    level: {
+      type: String,
+      enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+    },
+
+    ingredients: [String],
+
+    cuisine: {
+      type: String,
+      required: true
+    },
+
+    dishType: {
+      type: String,
+      enum: [
+        'breakfast',
+        'main_course',
+        'soup',
+        'snack',
+        'drink',
+        'dessert',
+        'other'
+      ]
+    },
+
+    image: {
+      type: String,
+      default: 'https://images.media-allrecipes.com/images/75131.jpg'
+    },
+
+    duration: {
+      type: Number,
+      min: 0
+    },
+
+    creator: {
+      type: String
+    }
+
+    // created: {
+    //   type: Date
+    //   // default: () => new Date();
+    //   // var today = new Date();
+    //   // var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    // }
+  },
+  { timestamps: true }
+);
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
